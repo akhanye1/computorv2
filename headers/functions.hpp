a31f135325a936f41641714911f44d6117bbc67d
Katleho <akhanye@student.wethinkcode.co.za>
2018-10-17 09:10:17 +0200

    preparing functions

---
+++ headers/functions.hpp
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+#include "../computorv.h"
+
+class Functions {
+    private:
+        string      functionName;
+        string      variable;
+        string      simplified;
+        float       placeHolder;
+};
+
+#endif
---
+++ src/functions.cpp
@@ -0,0 +1,2 @@
+#include "../computorv.h"
+
